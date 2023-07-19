225e1f89c82eebb986be3368c9b2630d102cb26a
lakshmidusanapudi <85738180+lakshmidusanapudi@users.noreply.github.com>
2023-07-19 10:51:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-function redrose(){
+function redflower(){
     document.getElementById("flower").src="https://www.pngmart.com/files/22/Flower-PNG-Isolated-File.png";
 }
-function whiterose(){
-    document.getElementById("flower").src="https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcm00NzYtcGYtczEyNy1iLTA2MC5wbmc.png?s=ffBjB8q-sV4yNpcsxGbr--xrmXabArauhVY8ykbJvRA";
-}
-function pinkrose()
+function fruit(){
+    document.getElementById("flower").src="https://www.pngmart.com/files/17/Fruits-PNG-Picture.png";}
+function sun()
 {
-    document.getElementById("flower").src="https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9zdGF0aWMvaW1hZ2VzL3dlYnNpdGUvMjAyMi0wNS9wZi1zMTI3LWFrLTY0MzJfMi5wbmc.png?s=cMBV61iLmxxnInu8JGmkG3lv5IwJDgzQ22vkBPwrGCU";
+    document.getElementById("flower").src="https://static.vecteezy.com/system/resources/previews/009/344/705/original/sun-transparent-background-free-png.png";
 }
